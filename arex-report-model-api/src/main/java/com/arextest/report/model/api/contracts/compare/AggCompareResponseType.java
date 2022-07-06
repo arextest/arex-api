6191db5691bc94625d5a267c6f00f47f41550c15
b_yu <b_yu@trip.com>
2022-07-06 15:07:06 +0800

    add the interface: /compare/quickCompare (#18)
    add the interface: /compare/quickCompare

---
+++ arex-report-model-api/src/main/java/com/arextest/report/model/api/contracts/compare/AggCompareResponseType.java
@@ -0,0 +1,7 @@
+package com.arextest.report.model.api.contracts.compare;
+
+/**
+ * Created by rchen9 on 2022/7/1.
+ */
+public class AggCompareResponseType {
+}
---
+++ arex-report-model-api/src/main/java/com/arextest/report/model/api/contracts/compare/MsgCombination.java
@@ -0,0 +1,12 @@
+package com.arextest.report.model.api.contracts.compare;
+
+import lombok.Data;
+
+/**
+ * Created by rchen9 on 2022/7/1.
+ */
+@Data
+public class MsgCombination {
+    private String baseMsg;
+    private String testMsg;
+}
