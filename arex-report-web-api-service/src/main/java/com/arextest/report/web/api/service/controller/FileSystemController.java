f146fb73f741ff0e3a228d17937c141023b5165d
binyu1005 <b_yu1005@hotmail.com>
2022-07-26 15:31:16 +0800

    add rename & remove workspace interfaces
    remove useless import

--- arex-report-web-api-service/src/main/java/com/arextest/report/web/api/service/controller/FileSystemController.java
+++ arex-report-web-api-service/src/main/java/com/arextest/report/web/api/service/controller/FileSystemController.java
@@ -36,7 +36,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import sun.misc.Request;
 
 import javax.annotation.Resource;
 
