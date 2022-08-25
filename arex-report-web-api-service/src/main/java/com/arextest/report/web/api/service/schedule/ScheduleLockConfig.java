5367771c6a052bb311eb0cc98303f57a946c3fcd
binyu1005 <b_yu1005@hotmail.com>
2022-08-25 14:57:30 +0800

    remove scheduleLock

--- arex-report-web-api-service/src/main/java/com/arextest/report/web/api/service/schedule/ScheduleLockConfig.java
+++ arex-report-web-api-service/src/main/java/com/arextest/report/web/api/service/schedule/ScheduleLockConfig.java
@@ -14,16 +14,16 @@ import javax.annotation.Resource;
  * @author b_yu
  * @since 2022/6/6
  */
-@Configuration
-public class ScheduleLockConfig {
-    private static final String TIME_WORK = "ScheduleLock";
-
-    @Resource
-    private MongoTemplate mongoTemplate;
-
-    @Bean
-    public LockProvider lockProvider() {
-        MongoCollection<Document> collection = mongoTemplate.getCollection(TIME_WORK);
-        return new MongoLockProvider(collection);
-    }
-}
+// @Configuration
+// public class ScheduleLockConfig {
+//     private static final String TIME_WORK = "ScheduleLock";
+//
+//     @Resource
+//     private MongoTemplate mongoTemplate;
+//
+//     @Bean
+//     public LockProvider lockProvider() {
+//         MongoCollection<Document> collection = mongoTemplate.getCollection(TIME_WORK);
+//         return new MongoLockProvider(collection);
+//     }
+// }
