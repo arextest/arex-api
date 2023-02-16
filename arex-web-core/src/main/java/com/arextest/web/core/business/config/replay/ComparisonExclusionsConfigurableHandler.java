39aac77c9d7a9ca01bdb63e16885df62d14f8a61
coryhh <44330697+coryhh@users.noreply.github.com>
2023-02-16 16:47:35 +0800

    feat:add global configuration to configuration query under collection (#124)

--- arex-web-core/src/main/java/com/arextest/web/core/business/config/replay/ComparisonExclusionsConfigurableHandler.java
+++ arex-web-core/src/main/java/com/arextest/web/core/business/config/replay/ComparisonExclusionsConfigurableHandler.java
@@ -1,9 +1,12 @@
 package com.arextest.web.core.business.config.replay;
 
+import com.arextest.web.core.business.config.application.ApplicationOperationConfigurableHandler;
 import com.arextest.web.core.repository.ConfigRepositoryProvider;
 import com.arextest.web.core.repository.FSInterfaceRepository;
+import com.arextest.web.model.contract.contracts.config.application.ApplicationOperationConfiguration;
 import com.arextest.web.model.contract.contracts.config.replay.ComparisonExclusionsConfiguration;
 import com.arextest.web.model.dto.filesystem.FSInterfaceDto;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -16,12 +19,15 @@ import java.util.List;
 @Component
 public class ComparisonExclusionsConfigurableHandler extends AbstractComparisonConfigurableHandler<ComparisonExclusionsConfiguration> {
     protected ComparisonExclusionsConfigurableHandler(@Autowired
-            ConfigRepositoryProvider<ComparisonExclusionsConfiguration> repositoryProvider) {
+                                                              ConfigRepositoryProvider<ComparisonExclusionsConfiguration> repositoryProvider) {
         super(repositoryProvider);
     }
 
     @Resource
     FSInterfaceRepository fsInterfaceRepository;
+
+    @Resource
+    ApplicationOperationConfigurableHandler applicationOperationConfigurableHandler;
 
     @Override
     public List<ComparisonExclusionsConfiguration> queryByInterfaceId(String interfaceId) {
@@ -30,6 +36,17 @@ public class ComparisonExclusionsConfigurableHandler extends AbstractComparisonC
         FSInterfaceDto fsInterfaceDto = fsInterfaceRepository.queryInterface(interfaceId);
         String operationId = fsInterfaceDto == null ? null : fsInterfaceDto.getOperationId();
 
-        return queryByOperationIdAndInterfaceId(interfaceId, operationId);
+        List<ComparisonExclusionsConfiguration> result =
+                this.queryByOperationIdAndInterfaceId(interfaceId, operationId);
+        if (StringUtils.isNotEmpty(operationId)) {
+            ApplicationOperationConfiguration applicationOperationConfiguration =
+                    applicationOperationConfigurableHandler.useResultById(operationId);
+            if (applicationOperationConfiguration != null) {
+                List<ComparisonExclusionsConfiguration> globalConfig =
+                        this.useResultAsList(applicationOperationConfiguration.getAppId(), null);
+                result.addAll(globalConfig);
+            }
+        }
+        return result;
     }
 }
