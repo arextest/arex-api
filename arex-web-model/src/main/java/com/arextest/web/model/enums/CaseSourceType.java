ec57aa58681141561df1d8723d374abda7cf2829
rchen9 <rchen9@trip.com>
2023-03-30 19:37:10 +0800

    feat:update the CaseSourceType

--- arex-web-model/src/main/java/com/arextest/web/model/enums/CaseSourceType.java
+++ arex-web-model/src/main/java/com/arextest/web/model/enums/CaseSourceType.java
@@ -4,6 +4,6 @@ package com.arextest.web.model.enums;
  * Created by rchen9 on 2023/3/22.
  */
 public class CaseSourceType {
-    public static final int MANUAL_CASE = 0;
-    public static final int REPLAY_CASE = 1;
+    public static final int MANUAL_CASE = 1;
+    public static final int REPLAY_CASE = 2;
 }
