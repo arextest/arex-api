2b9a53086ede00a733dacd3b0e42727454d203f0
rchen9 <rchen9@trip.com>
2023-04-10 19:20:41 +0800

    fix:the problem of the missing of "errorMessage"

--- arex-web-model-contract/src/main/java/com/arextest/web/model/contract/contracts/common/PlanItemStatistic.java
+++ arex-web-model-contract/src/main/java/com/arextest/web/model/contract/contracts/common/PlanItemStatistic.java
@@ -5,40 +5,41 @@ import lombok.Data;
 
 @Data
 public class PlanItemStatistic {
-    
+
     private String planItemId;
-    
+
     private String planId;
-    
+
     private String operationId;
-    
+
     private String operationName;
-    
+
     private String serviceName;
     private String appId;
 
-    
+
     private Integer status;
-    
+    private String errorMessage;
+
     private Long replayStartTime;
-    
+
     private Long replayEndTime;
-    
+
     private String sourceHost;
-    
+
     private String sourceEnv;
-    
+
     private String targetHost;
-    
+
     private String targetEnv;
-    
+
     private Integer caseSourceType;
-    
+
     private Long caseStartTime;
-    
+
     private Long caseEndTime;
 
-    
+
     private Integer totalCaseCount;
     private Integer errorCaseCount;
     private Integer successCaseCount;
