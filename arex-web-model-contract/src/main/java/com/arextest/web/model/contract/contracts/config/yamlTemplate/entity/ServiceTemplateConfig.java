309af8bca789773ebcdd2869c1dccbcb4bd7ef27
rchen9 <rchen9@trip.com>
2023-05-31 11:01:13 +0800

    fix: fix the linkage between yamltemplate and graphical configuration

--- arex-web-model-contract/src/main/java/com/arextest/web/model/contract/contracts/config/yamlTemplate/entity/ServiceTemplateConfig.java
+++ arex-web-model-contract/src/main/java/com/arextest/web/model/contract/contracts/config/yamlTemplate/entity/ServiceTemplateConfig.java
@@ -2,7 +2,11 @@ package com.arextest.web.model.contract.contracts.config.yamlTemplate.entity;
 
 import lombok.Data;
 
+import java.util.Collection;
 
+/**
+ * @see com.arextest.web.model.contract.contracts.config.record.ServiceCollectConfiguration
+ */
 @Data
 public class ServiceTemplateConfig {
 
@@ -10,7 +14,11 @@ public class ServiceTemplateConfig {
 
     private int allowDayOfWeeks;
 
+    private boolean timeMock;
+
     private String allowTimeOfDayFrom;
 
     private String allowTimeOfDayTo;
+
+    private Collection<String> excludeServiceOperationSet;
 }
