62fb3cdf06f28b48f3113fb6a010da27b490062f
rchen9 <rchen9@trip.com>
2023-06-05 15:21:17 +0800

    fix: the inconsistent of the totalCaseCount of plan between report and schedule

---
+++ arex-web-model-contract/src/main/java/com/arextest/web/model/contract/contracts/replay/UpdateReportInfoResponseType.java
@@ -0,0 +1,11 @@
+package com.arextest.web.model.contract.contracts.replay;
+
+import lombok.Data;
+
+/**
+ * Created by rchen9 on 2023/6/2.
+ */
+@Data
+public class UpdateReportInfoResponseType {
+    boolean success;
+}
