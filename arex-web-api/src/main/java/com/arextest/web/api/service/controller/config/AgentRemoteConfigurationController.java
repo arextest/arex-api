413ac8bfc585bf82a4e4eeddf0e1bb10b986a7dd
coryhh <jschenrui@163.com>
2023-08-04 15:05:08 +0800

    fix: add log about app registry

--- arex-web-api/src/main/java/com/arextest/web/api/service/controller/config/AgentRemoteConfigurationController.java
+++ arex-web-api/src/main/java/com/arextest/web/api/service/controller/config/AgentRemoteConfigurationController.java
@@ -1,4 +1,28 @@
 package com.arextest.web.api.service.controller.config;
+
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.http.client.utils.DateUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import com.arextest.common.model.response.Response;
 import com.arextest.common.utils.ResponseUtils;
 import com.arextest.web.common.LogUtils;
@@ -13,26 +37,9 @@ import com.arextest.web.model.contract.contracts.config.instance.AgentStatusRequ
 import com.arextest.web.model.contract.contracts.config.record.DynamicClassConfiguration;
 import com.arextest.web.model.contract.contracts.config.record.ServiceCollectConfiguration;
 import com.arextest.web.model.mapper.InstancesMapper;
+import com.google.common.collect.ImmutableMap;
+
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
-import org.apache.http.client.utils.DateUtils;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-
-import javax.annotation.PostConstruct;
-import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.List;
-import java.util.Set;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
-
 
 /**
  * @author jmo
@@ -73,10 +80,11 @@ public final class AgentRemoteConfigurationController {
         if (StringUtils.isEmpty(appId)) {
             return InvalidResponse.REQUESTED_APP_ID_IS_EMPTY;
         }
-        LogUtils.info(LOGGER, "from appId: {} , load config", appId);
+        LogUtils.info(LOGGER, ImmutableMap.of("appId", appId), "from appId: {} , load config", appId);
         ApplicationConfiguration applicationConfiguration = this.loadApplicationResult(request);
         if (applicationConfiguration == null) {
-            LogUtils.info(LOGGER, "from appId: {} load config resource not found", appId);
+            LogUtils.info(LOGGER, ImmutableMap.of("appId", appId), "from appId: {} load config resource not found",
+                appId);
             return ResponseUtils.resourceNotFoundResponse();
         }
         ServiceCollectConfiguration serviceCollectConfiguration = serviceCollectHandler.useResult(appId);
@@ -88,9 +96,9 @@ public final class AgentRemoteConfigurationController {
         InstancesConfiguration instancesConfiguration = InstancesMapper.INSTANCE.dtoFromContract(request);
         applicationInstancesConfigurableHandler.createOrUpdate(instancesConfiguration);
         List<InstancesConfiguration> instances = applicationInstancesConfigurableHandler.useResultAsList(appId,
-                serviceCollectConfiguration.getRecordMachineCountLimit());
+            serviceCollectConfiguration.getRecordMachineCountLimit());
         Set<String> recordingHosts =
-                instances.stream().map(InstancesConfiguration::getHost).collect(Collectors.toSet());
+            instances.stream().map(InstancesConfiguration::getHost).collect(Collectors.toSet());
         if (recordingHosts.contains(request.getHost())) {
             body.setTargetAddress(request.getHost());
         } else {
@@ -98,10 +106,12 @@ public final class AgentRemoteConfigurationController {
         }
         return ResponseUtils.successResponse(body);
     }
+
     @PostMapping("/agentStatus")
     @ResponseBody
     public ResponseEntity<String> agentStatus(HttpServletRequest httpServletRequest, HttpServletResponse response,
-                                              @RequestBody AgentStatusRequest request) {
+        @RequestBody AgentStatusRequest request) {
+        LogUtils.info(LOGGER, ImmutableMap.of("appId", request.getAppId()), "agentStatus request: {}", request);
         // update the instance
         InstancesConfiguration instancesConfiguration = InstancesMapper.INSTANCE.dtoFromContract(request);
         applicationInstancesConfigurableHandler.createOrUpdate(instancesConfiguration);
@@ -116,7 +126,7 @@ public final class AgentRemoteConfigurationController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
         String modifiedTime = EMPTY_TIME;
-        if (serviceConfig.getModifiedTime()!=null) {
+        if (serviceConfig.getModifiedTime() != null) {
             modifiedTime = DateUtils.formatDate(serviceConfig.getModifiedTime());
         }
         HttpStatus httpStatus = HttpStatus.OK;
@@ -126,10 +136,11 @@ public final class AgentRemoteConfigurationController {
             httpStatus = HttpStatus.NOT_MODIFIED;
         }
 
-            // 200 OK response
+        // 200 OK response
         response.setHeader("Last-Modified", modifiedTime);
         return new ResponseEntity<>(httpStatus);
     }
+
     private void delayUpdateApplicationService(AgentRemoteConfigurationRequest request) {
         executorService.schedule(new UpdateApplicationRunnable(request), delayUpdateServiceSeconds, TimeUnit.SECONDS);
     }
