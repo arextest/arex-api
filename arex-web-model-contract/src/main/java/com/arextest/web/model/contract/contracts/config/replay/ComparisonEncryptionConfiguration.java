c7df2509b29b3d98851e715a42c6083ce5b6bd55
zhitaogan <owengan@bu.edu>
2023-08-08 15:58:04 +0800

    feat: "adding crud for encryption"

---
+++ arex-web-model-contract/src/main/java/com/arextest/web/model/contract/contracts/config/replay/ComparisonEncryptionConfiguration.java
@@ -0,0 +1,28 @@
+package com.arextest.web.model.contract.contracts.config.replay;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.util.CollectionUtils;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+public class ComparisonEncryptionConfiguration extends AbstractComparisonDetailsConfiguration {
+    List<String> path;
+    String methodName;
+
+    @Override
+    public void validParameters() throws Exception {
+        super.validParameters();
+        if (CollectionUtils.isEmpty(path) || methodName.isEmpty()) {
+            throw new Exception("path or methodName cannot be empty");
+        }
+    }
+
+
+
+}
