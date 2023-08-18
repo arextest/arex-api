10a350f6ae10eddc16fe4c5042a98b98fa4a3a99
zhitaogan <owengan@bu.edu>
2023-08-18 16:08:12 +0800

    feat: "add route for removing instances and add update time method so that everytime instances are removed or dynamic class is modified, recordService update time is updated accordingly"

--- arex-web-api/src/main/java/com/arextest/web/api/service/controller/config/ApplicationInstancesConfigurableController.java
+++ arex-web-api/src/main/java/com/arextest/web/api/service/controller/config/ApplicationInstancesConfigurableController.java
@@ -1,10 +1,20 @@
 package com.arextest.web.api.service.controller.config;
 
+import com.arextest.common.model.response.Response;
+import com.arextest.web.core.business.config.AbstractConfigurableHandler;
+import com.arextest.web.core.business.config.ConfigurableHandler;
 import com.arextest.web.core.business.config.application.ApplicationInstancesConfigurableHandler;
+import com.arextest.web.model.contract.contracts.common.enums.ModifyType;
 import com.arextest.web.model.contract.contracts.config.application.InstancesConfiguration;
+import com.arextest.web.model.contract.contracts.config.record.ServiceCollectConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
 
 /**
  * Created by rchen9 on 2022/9/30.
@@ -17,4 +27,14 @@ public class ApplicationInstancesConfigurableController extends AbstractConfigur
         super(configurableHandler);
     }
 
+    @Resource
+    private ServiceCollectConfigurableController serviceCollectConfigurableController;
+
+    @Override
+    @ResponseBody
+    public Response modify(@PathVariable ModifyType modifyType,@RequestBody InstancesConfiguration configuration) throws Exception {
+        serviceCollectConfigurableController.updateServiceCollectTime(configuration.getAppId());
+
+        return super.modify(modifyType, configuration);
+    }
 }
