299e4d80fb68074c26e457c481ee690169af5e00
wildeslam <3160104749@zju.edu.cn>
2023-08-30 16:51:36 +0800

    feat: rerun pt2

--- arex-web-core/src/main/java/com/arextest/web/core/repository/mongo/ReportPlanStatisticRepositoryImpl.java
+++ arex-web-core/src/main/java/com/arextest/web/core/repository/mongo/ReportPlanStatisticRepositoryImpl.java
@@ -308,7 +308,8 @@ public class ReportPlanStatisticRepositoryImpl implements ReportPlanStatisticRep
     public ReportPlanStatisticDto changePlanStatus(String planId,
                                                    Integer status,
                                                    Integer totalCaseCount,
-                                                   String errorMessage) {
+                                                   String errorMessage,
+                                                   boolean rerun) {
         if (planId == null || planId == "") {
             return null;
         }
@@ -322,7 +323,10 @@ public class ReportPlanStatisticRepositoryImpl implements ReportPlanStatisticRep
         if (errorMessage != null) {
             update.set(ERROR_MESSAGE, errorMessage);
         }
-        update.set(REPLAY_END_TIME, System.currentTimeMillis());
+        // rerun does not update end time
+        if (!rerun) {
+            update.set(REPLAY_END_TIME, System.currentTimeMillis());
+        }
         if (update.getUpdateObject().keySet().size() == 0) {
             return null;
         }
