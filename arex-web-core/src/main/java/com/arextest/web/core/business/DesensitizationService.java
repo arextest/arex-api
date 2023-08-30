993e512330a51a158e430f84fc2a8f9b041997a8
Nathan Mo <54135657+QizhengMo@users.noreply.github.com>
2023-08-30 14:10:39 +0800

    chore: remove jar validation (#255)

--- arex-web-core/src/main/java/com/arextest/web/core/business/DesensitizationService.java
+++ arex-web-core/src/main/java/com/arextest/web/core/business/DesensitizationService.java
@@ -24,26 +24,7 @@ public class DesensitizationService {
     @Resource
     private DesensitizationJarRepository jarRepository;
 
-    public static class DesensitizationJarValidationException extends RuntimeException {
-        public DesensitizationJarValidationException(ErrType errType) {
-            super(errType.message);
-        }
-    }
-
-    enum ErrType {
-        URL_MALFORMED("Remote Jar Url is malformed"),
-        SERVICE_NOTFOUND("Remote Jar can not load DataDesensitization interface implementation"),
-        SERVICE_OVERLOAD("Remote Jar found more than one DataDesensitization interface implementation"),
-        ;
-        private final String message;
-
-        ErrType(String message) {
-            this.message = message;
-        }
-    }
-
-    public boolean saveJar(String uri, String remark) throws DesensitizationJarValidationException {
-        validateJar(uri);
+    public boolean saveJar(String uri, String remark) {
         jarRepository.deleteAll();
         DesensitizationJarDto jarDto = new DesensitizationJarDto();
         jarDto.setJarUrl(uri);
@@ -57,24 +38,5 @@ public class DesensitizationService {
 
     public List<DesensitizationJar> listAllJars() {
         return jarRepository.queryAll().stream().map(DesensitizationJarMapper.INSTANCE::contractFromDto).collect(Collectors.toList());
-    }
-
-    private void validateJar(String uri) throws DesensitizationJarValidationException {
-        if (!uri.startsWith("http")) {
-            throw new DesensitizationJarValidationException(ErrType.URL_MALFORMED);
-        }
-
-        try {
-            RemoteJarClassLoader loader = RemoteJarLoaderUtils.loadJar(uri);
-            List<DataDesensitization> service = RemoteJarLoaderUtils.loadService(DataDesensitization.class, loader);
-            if (service.isEmpty()) {
-                throw new DesensitizationJarValidationException(ErrType.SERVICE_NOTFOUND);
-            } else if (service.size() != 1) {
-                throw new DesensitizationJarValidationException(ErrType.SERVICE_OVERLOAD);
-            }
-        } catch (MalformedURLException e) {
-            throw new DesensitizationJarValidationException(ErrType.URL_MALFORMED);
-        }
-
     }
 }
