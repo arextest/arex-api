234c4e57dc4f14c601a91c205cf0c1d765e9d884
ZhitaoGan <98361212+ZhitaoGan@users.noreply.github.com>
2023-09-15 14:06:32 +0800

    Feat serialize update backup (#273)
    * feat: "adding Serialize update"
    
    * feat: "change the version of storage package"
    
    * feat: "change the version of every report package"

--- arex-web-api/src/main/java/com/arextest/web/api/service/controller/config/ServiceCollectConfigurableController.java
+++ arex-web-api/src/main/java/com/arextest/web/api/service/controller/config/ServiceCollectConfigurableController.java
@@ -1,10 +1,19 @@
 package com.arextest.web.api.service.controller.config;
 
+import com.arextest.common.model.response.Response;
+import com.arextest.common.utils.ResponseUtils;
 import com.arextest.config.model.dto.record.ServiceCollectConfiguration;
 import com.arextest.web.core.business.config.ConfigurableHandler;
+import com.arextest.web.core.business.config.record.ServiceCollectConfigurableHandler;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
 
 /**
  * @author jmo
@@ -15,5 +24,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public final class ServiceCollectConfigurableController extends AbstractConfigurableController<ServiceCollectConfiguration> {
     public ServiceCollectConfigurableController(@Autowired ConfigurableHandler<ServiceCollectConfiguration> configurableHandler) {
         super(configurableHandler);
+    }
+    @Resource
+    private ServiceCollectConfigurableHandler serviceCollectConfigurableHandler;
+
+    @PostMapping("/updateCustomConfig")
+    @ResponseBody
+    public Response updateCustomConfig(@RequestBody ServiceCollectConfiguration serviceCollectConfiguration) {
+        if (serviceCollectConfiguration == null|| StringUtils.isBlank(serviceCollectConfiguration.getAppId())){
+            return ResponseUtils.parameterInvalidResponse("The request body is null or appId is blank");
+        }
+        return ResponseUtils.successResponse(serviceCollectConfigurableHandler.update(serviceCollectConfiguration));
     }
 }
