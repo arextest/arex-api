93bd1d276b1339aaa856a85c2b2a9c068b32fcc8
yushuwang <yushuwang@trip.com>
2023-10-07 17:04:46 +0800

    feat:keyword

--- arex-web-core/src/main/java/com/arextest/web/core/repository/mongo/UserRepositoryImpl.java
+++ arex-web-core/src/main/java/com/arextest/web/core/repository/mongo/UserRepositoryImpl.java
@@ -8,14 +8,10 @@ import com.arextest.web.model.dao.mongodb.UserCollection;
 import com.arextest.web.model.dto.UserDto;
 import com.arextest.web.model.enums.UserStatusType;
 import com.arextest.web.model.mapper.UserMapper;
-import com.mongodb.BasicDBObject;
 import com.mongodb.client.result.UpdateResult;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.mongodb.core.FindAndModifyOptions;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.aggregation.Aggregation;
-import org.springframework.data.mongodb.core.aggregation.AggregationResults;
-import org.springframework.data.mongodb.core.aggregation.ProjectionOperation;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
@@ -110,23 +106,15 @@ public class UserRepositoryImpl implements UserRepository {
 
     @Override
     public List<UserDto> queryVerifiedUseWithKeyword(String keyword) {
-        ProjectionOperation projectionOperation = Aggregation.project(ModelBase.Fields.id, UserCollection.Fields.userName);
         Criteria criteria = Criteria.where(UserCollection.Fields.status).ne(UserStatusType.GUEST);
         if (keyword != null) {
             criteria.andOperator(Criteria.where(UserCollection.Fields.userName).regex(String.format(LIKE_QUERY_PATTERN, keyword)));
         }
-        Aggregation aggregation = Aggregation.newAggregation(
-            Aggregation.match(criteria),
-            projectionOperation,
-            Aggregation.limit(DEFAULT_LIMIT)
-        );
-        AggregationResults<BasicDBObject> aggregate = mongoTemplate.aggregate(aggregation, UserCollection.class, BasicDBObject.class);
-        return aggregate.getMappedResults().stream().map(this::covertToBaseUserDto).collect(Collectors.toList());
-    }
-
-    private UserDto covertToBaseUserDto(BasicDBObject basicDBObject) {
-        UserDto userDto = new UserDto();
-        userDto.setUserName(basicDBObject.getString(UserCollection.Fields.userName));
-        return userDto;
+        Query query = new Query(criteria);
+        query.fields().include(UserCollection.Fields.userName).exclude(ModelBase.Fields.id);
+        query.limit(DEFAULT_LIMIT);
+        return mongoTemplate.find(query, UserCollection.class).stream()
+            .map(UserMapper.INSTANCE::dtoFromDao)
+            .collect(Collectors.toList());
     }
 }
