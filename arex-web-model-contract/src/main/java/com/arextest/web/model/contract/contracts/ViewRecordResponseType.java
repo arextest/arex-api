8c84ca06253acd41db7c0d054a0e48cd4cdb4d9f
yushuwang <yushuwang@trip.com>
2023-10-16 16:47:56 +0800

    chore:update version

---
+++ arex-web-model-contract/src/main/java/com/arextest/web/model/contract/contracts/ViewRecordResponseType.java
@@ -0,0 +1,19 @@
+package com.arextest.web.model.contract.contracts;
+
+import com.arextest.model.mock.AREXMocker;
+import com.arextest.model.response.Response;
+import com.arextest.model.response.ResponseStatusType;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * @author wildeslam.
+ * @create 2023/10/16 16:40
+ */
+@Data
+public class ViewRecordResponseType implements Response {
+    private ResponseStatusType responseStatusType;
+    private List<AREXMocker> recordResult;
+    private boolean desensitized;
+}
