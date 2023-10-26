a511ccf6b0dfc75dc6ab2d9f1b0ee70b22cec72e
yushuwang <yushuwang@trip.com>
2023-10-26 14:16:28 +0800

    feat: change properties key

--- arex-web-api/src/main/java/com/arextest/web/api/service/controller/ReplayQueryController.java
+++ arex-web-api/src/main/java/com/arextest/web/api/service/controller/ReplayQueryController.java
@@ -31,7 +31,7 @@ import com.arextest.web.model.contract.contracts.ViewRecordResponseType;
 @RequestMapping("/api/replay/query")
 public class ReplayQueryController {
 
-    @Value("${arex.storage.viewRecord.url}")
+    @Value("${arex.storage.view.record.url}")
     private String viewRecordUrl;
 
     @ResponseBody
