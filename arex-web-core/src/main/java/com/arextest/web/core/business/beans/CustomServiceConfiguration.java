314a7fde26c869e1f038fa1d3310a90bc738b2f7
rchen9 <rchen9@trip.com>
2023-10-31 11:34:11 +0800

    style: code style

--- arex-web-core/src/main/java/com/arextest/web/core/business/beans/CustomServiceConfiguration.java
+++ arex-web-core/src/main/java/com/arextest/web/core/business/beans/CustomServiceConfiguration.java
@@ -3,10 +3,6 @@ package com.arextest.web.core.business.beans;
 import com.arextest.config.model.dto.application.ApplicationServiceConfiguration;
 import com.arextest.web.core.business.ConfigLoadService;
 import com.arextest.web.core.business.config.ConfigurableHandler;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
 import com.arextest.web.core.business.config.replay.ComparisonEncryptionConfigurableHandler;
 import com.arextest.web.core.business.config.replay.ComparisonExclusionsConfigurableHandler;
 import com.arextest.web.core.business.config.replay.ComparisonIgnoreCategoryConfigurableHandler;
@@ -15,32 +11,36 @@ import com.arextest.web.core.business.config.replay.ComparisonListSortConfigurab
 import com.arextest.web.core.business.config.replay.ComparisonReferenceConfigurableHandler;
 import com.arextest.web.core.business.config.replay.ComparisonSummaryService;
 import com.arextest.web.core.repository.AppContractRepository;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 
 @Configuration
 public class CustomServiceConfiguration {
 
-    @Bean
-    @ConditionalOnMissingBean(ComparisonSummaryService.class)
-    public ComparisonSummaryService comparisonSummaryService(
-        ComparisonExclusionsConfigurableHandler exclusionsConfigurableHandler,
-        ComparisonInclusionsConfigurableHandler inclusionsConfigurableHandler,
-        ComparisonEncryptionConfigurableHandler encryptionConfigurableHandler,
-        ComparisonReferenceConfigurableHandler referenceConfigurableHandler,
-        ComparisonListSortConfigurableHandler listSortConfigurableHandler,
-        ConfigurableHandler<ApplicationServiceConfiguration> applicationServiceConfigurationConfigurableHandler,
-        AppContractRepository appContractRepository,
-        ComparisonIgnoreCategoryConfigurableHandler ignoreCategoryConfigurableHandler) {
-        return new ComparisonSummaryService(exclusionsConfigurableHandler, inclusionsConfigurableHandler,
-            encryptionConfigurableHandler, referenceConfigurableHandler, listSortConfigurableHandler,
-            applicationServiceConfigurationConfigurableHandler, appContractRepository,
-            ignoreCategoryConfigurableHandler);
-    }
+  @Bean
+  @ConditionalOnMissingBean(ComparisonSummaryService.class)
+  public ComparisonSummaryService comparisonSummaryService(
+      ComparisonExclusionsConfigurableHandler exclusionsConfigurableHandler,
+      ComparisonInclusionsConfigurableHandler inclusionsConfigurableHandler,
+      ComparisonEncryptionConfigurableHandler encryptionConfigurableHandler,
+      ComparisonReferenceConfigurableHandler referenceConfigurableHandler,
+      ComparisonListSortConfigurableHandler listSortConfigurableHandler,
+      ConfigurableHandler<ApplicationServiceConfiguration> applicationServiceConfigurationConfigurableHandler,
+      AppContractRepository appContractRepository,
+      ComparisonIgnoreCategoryConfigurableHandler ignoreCategoryConfigurableHandler) {
+    return new ComparisonSummaryService(exclusionsConfigurableHandler,
+        inclusionsConfigurableHandler,
+        encryptionConfigurableHandler, referenceConfigurableHandler, listSortConfigurableHandler,
+        applicationServiceConfigurationConfigurableHandler, appContractRepository,
+        ignoreCategoryConfigurableHandler);
+  }
 
-    @Bean
-    @ConditionalOnMissingBean(ConfigLoadService.class)
-    public ConfigLoadService configLoadService(Environment environment) {
-        return new DefaultConfigLoadService(environment);
-    }
+  @Bean
+  @ConditionalOnMissingBean(ConfigLoadService.class)
+  public ConfigLoadService configLoadService(Environment environment) {
+    return new DefaultConfigLoadService(environment);
+  }
 
 }
