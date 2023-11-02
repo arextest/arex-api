13483b65ea91cfaf32a7e699f515420f06e95d80
yushuwang <yushuwang@trip.com>
2023-11-02 20:21:14 +0800

    feat: visibility isolate

--- arex-web-api/src/main/java/com/arextest/web/api/service/controller/config/ApplicationConfigurableController.java
+++ arex-web-api/src/main/java/com/arextest/web/api/service/controller/config/ApplicationConfigurableController.java
@@ -1,8 +1,10 @@
 package com.arextest.web.api.service.controller.config;
 
 import com.arextest.common.model.response.Response;
+import com.arextest.common.utils.JwtUtil;
 import com.arextest.common.utils.ResponseUtils;
 import com.arextest.config.model.dto.application.ApplicationConfiguration;
+import com.arextest.model.replay.AppVisibilityLevelEnum;
 import com.arextest.web.core.business.config.ConfigurableHandler;
 import com.arextest.web.core.business.config.replay.ScheduleConfigurableHandler;
 import com.arextest.web.model.contract.contracts.config.replay.ScheduleConfiguration;
@@ -12,12 +14,15 @@ import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 /**
  * @author jmo
@@ -39,15 +44,23 @@ public class ApplicationConfigurableController extends
   @GetMapping("/regressionList")
   @ResponseBody
   public Response regressionList() {
-    List<ApplicationConfiguration> sourceMap = this.configurableHandler.useResultAsList();
+    ServletRequestAttributes requestAttributes =
+        (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+    HttpServletRequest request = requestAttributes.getRequest();
+    String accessToken = request.getHeader("access-token");
+    String userName = JwtUtil.getUserName(accessToken);
+    List<ApplicationConfiguration> sourceList = this.configurableHandler.useResultAsList();
+    sourceList = sourceList.stream()
+        .filter(applicationConfiguration -> visibilityCheck(applicationConfiguration, userName))
+        .collect(Collectors.toList());
     Map<String, ScheduleConfiguration> scheduleMap = scheduleHandler.useResultAsList().stream()
         .collect(
             Collectors.toMap(ScheduleConfiguration::getAppId, Function.identity(),
                 (oldValue, newValue) -> newValue));
 
-    List<ApplicationRegressionView> viewList = new ArrayList<>(sourceMap.size());
+    List<ApplicationRegressionView> viewList = new ArrayList<>(sourceList.size());
     ApplicationRegressionView view;
-    for (ApplicationConfiguration application : sourceMap) {
+    for (ApplicationConfiguration application : sourceList) {
       view = new ApplicationRegressionView();
       view.setApplication(application);
       ScheduleConfiguration configuration = scheduleMap.get(application.getAppId());
@@ -58,6 +71,15 @@ public class ApplicationConfigurableController extends
       viewList.add(view);
     }
     return ResponseUtils.successResponse(viewList);
+  }
+
+  private boolean visibilityCheck(ApplicationConfiguration applicationConfiguration,
+      String userName) {
+    return applicationConfiguration.getVisibilityLevel()
+        == AppVisibilityLevelEnum.ALL_VISIBLE.getCode()
+        || (applicationConfiguration.getVisibilityLevel()
+        == AppVisibilityLevelEnum.OWNER_VISIBLE.getCode()
+        && applicationConfiguration.getOwners().contains(userName));
   }
 
   @Data
