f9823de5d247edc590c91f21e5502d42d727cc2a
yushuwang <yushuwang@trip.com>
2023-11-16 10:39:19 +0800

    feat: get pom version at vi/health interface

--- arex-web-api/src/main/java/com/arextest/web/api/service/controller/CheckHealthController.java
+++ arex-web-api/src/main/java/com/arextest/web/api/service/controller/CheckHealthController.java
@@ -3,11 +3,15 @@ package com.arextest.web.api.service.controller;
 import com.arextest.common.model.response.Response;
 import com.arextest.common.utils.ResponseUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.FileReader;
 
 @Slf4j
 @Controller
@@ -18,6 +22,18 @@ public class CheckHealthController {
   @GetMapping("/health")
   @ResponseBody
   public Response checkHealth() {
-    return ResponseUtils.successResponse(true);
+
+    return ResponseUtils.successResponse(getVersion());
   }
+
+  private static String getVersion() {
+    try {
+      MavenXpp3Reader reader = new MavenXpp3Reader();
+      Model model = reader.read(new FileReader("pom.xml"));
+      return model.getVersion();
+    } catch (Exception e) {
+      return "error";
+    }
+  }
+
 }
