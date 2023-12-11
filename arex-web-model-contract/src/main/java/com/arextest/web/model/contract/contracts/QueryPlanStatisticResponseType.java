82c1aed545783f4e53e26088e7e2d75d61bd94ba
yushuwang <yushuwang@trip.com>
2023-12-11 14:48:51 +0800

    feat:add queryPlanStatistic for backend

--- arex-web-model-contract/src/main/java/com/arextest/web/model/contract/contracts/QueryPlanStatisticResponseType.java
+++ arex-web-model-contract/src/main/java/com/arextest/web/model/contract/contracts/QueryPlanStatisticResponseType.java
@@ -1,5 +1,7 @@
 package com.arextest.web.model.contract.contracts;
 
+import com.arextest.model.response.Response;
+import com.arextest.model.response.ResponseStatusType;
 import com.arextest.web.model.contract.contracts.common.PlanStatistic;
 import lombok.Data;
 
@@ -8,6 +10,7 @@ import lombok.Data;
  * @create 2023/12/8 16:41
  */
 @Data
-public class QueryPlanStatisticResponseType {
+public class QueryPlanStatisticResponseType implements Response {
     private PlanStatistic planStatistic;
+    private ResponseStatusType responseStatusType;
 }
