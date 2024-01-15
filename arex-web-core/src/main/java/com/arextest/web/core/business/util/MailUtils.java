dd13885e0c37c93508d2dc6c576708a2d2f9261f
rchen9 <rchen9@trip.com>
2024-01-15 18:57:26 +0800

    feat: ignore category

--- arex-web-core/src/main/java/com/arextest/web/core/business/util/MailUtils.java
+++ arex-web-core/src/main/java/com/arextest/web/core/business/util/MailUtils.java
@@ -1,13 +1,21 @@
 package com.arextest.web.core.business.util;
 
-import com.arextest.web.common.HttpUtils;
 import com.arextest.web.common.LogUtils;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.Proxy.Type;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @Slf4j
 @Component
@@ -23,10 +31,23 @@ public class MailUtils {
       sendEmailUrl = arexEmailDomain + "/email/sendEmail";
     }
     try {
+      sendEmailUrl = "http://mail.arextest.com/email/sendEmail";
       SendMailRequest request = new SendMailRequest(mailBox, subject, htmlMsg, CONTENT_TYPE, type);
-      ResponseEntity<SendMailResponse> response = HttpUtils.post(sendEmailUrl, request,
-          SendMailResponse.class);
-      return response.getBody().getData().getSuccess();
+//      ResponseEntity<SendMailResponse> response = HttpUtils.post(sendEmailUrl, request,
+//          SendMailResponse.class);
+      SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+
+      Proxy proxy = new Proxy(Type.HTTP,
+          new InetSocketAddress("ntproxy.qa.nt.ctripcorp.com", 8080));
+      requestFactory.setProxy(proxy);
+
+      RestTemplate restTemplate = new RestTemplate(requestFactory);
+      HttpHeaders httpHeaders = new HttpHeaders();
+      httpHeaders.setContentType(MediaType.APPLICATION_JSON_UTF8);
+      HttpEntity<SendMailRequest> requestEntity = new HttpEntity<>(request, httpHeaders);
+      ResponseEntity<SendMailResponse> exchange = restTemplate.exchange(sendEmailUrl,
+          HttpMethod.POST, requestEntity, SendMailResponse.class);
+      return exchange.getBody().getData().getSuccess();
     } catch (Exception e) {
       LogUtils.error(LOGGER, "Failed to send email. type:{}", type);
       return false;
