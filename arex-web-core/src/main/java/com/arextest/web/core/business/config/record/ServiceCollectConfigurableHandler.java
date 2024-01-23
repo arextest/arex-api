023a19540de25d84735b5d2ef3beda0f034368d7
rchen9 <rchen9@trip.com>
2024-01-23 14:54:47 +0800

    feat: remove "arex.config.default.service"

--- arex-web-core/src/main/java/com/arextest/web/core/business/config/record/ServiceCollectConfigurableHandler.java
+++ arex-web-core/src/main/java/com/arextest/web/core/business/config/record/ServiceCollectConfigurableHandler.java
@@ -1,5 +1,8 @@
 package com.arextest.web.core.business.config.record;
 
+import com.arextest.config.model.dto.record.ServiceCollectConfiguration;
+import com.arextest.config.repository.ConfigRepositoryProvider;
+import com.arextest.web.core.business.config.AbstractConfigurableHandler;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
@@ -7,9 +10,8 @@ import java.util.Set;
 import javax.annotation.Resource;
 
 import org.apache.commons.collections4.CollectionUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 import com.arextest.config.model.dto.record.ServiceCollectConfiguration;
@@ -27,9 +29,6 @@ import lombok.extern.slf4j.Slf4j;
 public final class ServiceCollectConfigurableHandler
     extends AbstractMultiEnvConfigHandler<ServiceCollectConfiguration> {
 
-  @Resource
-  private ServiceCollectConfiguration globalDefaultConfiguration;
-
   protected ServiceCollectConfigurableHandler(
       @Autowired MultiEnvConfigRepositoryProvider<ServiceCollectConfiguration> repositoryProvider) {
     super(repositoryProvider);
@@ -37,20 +36,8 @@ public final class ServiceCollectConfigurableHandler
 
   @Override
   protected List<ServiceCollectConfiguration> createFromGlobalDefault(String appId) {
-    ServiceCollectConfiguration serviceCollectConfiguration = new ServiceCollectConfiguration();
-    serviceCollectConfiguration.setAppId(appId);
-    serviceCollectConfiguration.setSampleRate(globalDefaultConfiguration.getSampleRate());
-    serviceCollectConfiguration.setAllowDayOfWeeks(globalDefaultConfiguration.getAllowDayOfWeeks());
-    serviceCollectConfiguration.setTimeMock(globalDefaultConfiguration.isTimeMock());
-    serviceCollectConfiguration.setAllowTimeOfDayFrom(
-        globalDefaultConfiguration.getAllowTimeOfDayFrom());
-    serviceCollectConfiguration.setAllowTimeOfDayTo(
-        globalDefaultConfiguration.getAllowTimeOfDayTo());
-    serviceCollectConfiguration
-        .setRecordMachineCountLimit(
-            globalDefaultConfiguration.getRecordMachineCountLimit() == null ? 1
-                : globalDefaultConfiguration.getRecordMachineCountLimit());
-    return Collections.singletonList(serviceCollectConfiguration);
+    LOGGER.error("query and find serviceCollectConfiguration is empty, appId:{}", appId);
+    return Collections.emptyList();
   }
 
   @Override
@@ -62,30 +49,8 @@ public final class ServiceCollectConfigurableHandler
   protected void mergeGlobalDefaultSettings(ServiceCollectConfiguration source) {
   }
 
-  @Override
-  protected boolean shouldMergeGlobalDefault() {
-    return true;
-  }
-
-  private <T> Set<T> mergeValues(Set<T> source, Set<T> globalValues) {
-    if (CollectionUtils.isEmpty(globalValues)) {
-      return source;
-    }
-    if (CollectionUtils.isEmpty(source)) {
-      return globalValues;
-    }
-    source.addAll(globalValues);
-    return source;
-  }
-
   public void updateServiceCollectTime(String appId) {
     ServiceCollectConfiguration serviceCollectConfiguration = this.useResult(appId);
     this.update(serviceCollectConfiguration);
-  }
-
-  @Configuration
-  @ConfigurationProperties(prefix = "arex.config.default.service.collect")
-  static class GlobalServiceCollectConfiguration extends ServiceCollectConfiguration {
-
   }
 }
