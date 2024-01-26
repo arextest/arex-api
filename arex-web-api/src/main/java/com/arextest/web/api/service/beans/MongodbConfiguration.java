9e29ba8ee65f5487d74b424bc3606590432673de
wildeslam <yushuwang@trip.com>
2024-01-26 11:14:26 +0800

    perf: async

--- arex-web-api/src/main/java/com/arextest/web/api/service/beans/MongodbConfiguration.java
+++ arex-web-api/src/main/java/com/arextest/web/api/service/beans/MongodbConfiguration.java
@@ -1,26 +1,17 @@
 package com.arextest.web.api.service.beans;
 
 import com.arextest.web.common.LogUtils;
-import com.arextest.web.core.repository.mongo.util.MongoHelper;
+import com.arextest.web.core.repository.mongo.util.OldDataCleaner;
 import com.arextest.web.model.dao.mongodb.AppContractCollection;
-import com.arextest.web.model.dao.mongodb.ConfigComparisonIgnoreCategoryCollection;
 import com.arextest.web.model.dao.mongodb.LogsCollection;
 import com.arextest.web.model.dao.mongodb.ReplayScheduleConfigCollection;
 import com.arextest.web.model.dao.mongodb.ReportPlanItemStatisticCollection;
 import com.arextest.web.model.dao.mongodb.ReportPlanStatisticCollection;
-import com.arextest.web.model.dao.mongodb.entity.CategoryDetailDao;
 import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
-
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.collections4.CollectionUtils;
 import org.bson.codecs.configuration.CodecRegistries;
 import org.bson.codecs.configuration.CodecRegistry;
 import org.bson.codecs.pojo.PojoCodecProvider;
@@ -39,8 +30,8 @@ import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
 import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
 import org.springframework.data.mongodb.core.index.Index;
 import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
+
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Configuration
@@ -78,7 +69,6 @@ public class MongodbConfiguration {
     return null;
   }
 
-  @Lazy
   @Bean
   @ConditionalOnMissingBean
   public MongoTemplate mongoTemplate() {
@@ -87,9 +77,15 @@ public class MongodbConfiguration {
         new MongoMappingContext());
     converter.setTypeMapper(new DefaultMongoTypeMapper(null));
     MongoTemplate template = new MongoTemplate(this.mongoDbFactory(), converter);
-    cleanOldData(template);
     initIndicesAfterStartup(template);
     return template;
+  }
+
+  @Bean
+  @Lazy(false)
+  @ConditionalOnMissingBean
+  public OldDataCleaner oldDataCleaner(MongoTemplate mongoTemplate ) {
+    return new OldDataCleaner(mongoTemplate);
   }
 
   public void initIndicesAfterStartup(MongoTemplate mongoTemplate) {
@@ -126,40 +122,5 @@ public class MongodbConfiguration {
 
   }
 
-  public void cleanOldData(MongoTemplate mongoTemplate) {
-      cleanConfigComparisonIgnoreCategoryCollection(mongoTemplate);
-  }
-
-  private void cleanConfigComparisonIgnoreCategoryCollection(MongoTemplate mongoTemplate) {
-    Query query = Query.query(Criteria.where(ConfigComparisonIgnoreCategoryCollection.Fields.ignoreCategory).ne(null));
-    List<ConfigComparisonIgnoreCategoryCollection> oldData =
-        mongoTemplate.findAllAndRemove(query, ConfigComparisonIgnoreCategoryCollection.class).stream()
-            .filter(config -> CollectionUtils.isNotEmpty(config.getIgnoreCategory()))
-            .collect(Collectors.toList());
-
-    List<ConfigComparisonIgnoreCategoryCollection> newData = new ArrayList<>();
-    oldData.forEach(oldConfig -> {
-      oldConfig.getIgnoreCategory().forEach(ignoreCategory -> {
-        ConfigComparisonIgnoreCategoryCollection newConfigItem = convertToNewConfig(oldConfig);
-        CategoryDetailDao categoryDetailDao = new CategoryDetailDao();
-        categoryDetailDao.setOperationType(ignoreCategory);
-        newConfigItem.setIgnoreCategoryDetail(categoryDetailDao);
-
-        newData.add(newConfigItem);
-      });
-    });
-    mongoTemplate.insertAll(newData);
-  }
-
-  private ConfigComparisonIgnoreCategoryCollection convertToNewConfig(
-      ConfigComparisonIgnoreCategoryCollection oldConfig) {
-    ConfigComparisonIgnoreCategoryCollection newConfig = new ConfigComparisonIgnoreCategoryCollection();
-    MongoHelper.initInsertObject(newConfig);
-    newConfig.setAppId(oldConfig.getAppId());
-    newConfig.setOperationId(oldConfig.getOperationId());
-    newConfig.setExpirationType(oldConfig.getExpirationType());
-    newConfig.setExpirationDate(oldConfig.getExpirationDate());
-    return newConfig;
-  }
 
 }
---
+++ arex-web-core/src/main/java/com/arextest/web/core/repository/mongo/util/OldDataCleaner.java
@@ -0,0 +1,71 @@
+package com.arextest.web.core.repository.mongo.util;
+
+import com.arextest.web.model.dao.mongodb.ConfigComparisonIgnoreCategoryCollection;
+import com.arextest.web.model.dao.mongodb.entity.CategoryDetailDao;
+import org.apache.commons.collections4.CollectionUtils;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Repository;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+
+/**
+ * @author wildeslam.
+ * @create 2024/1/26 11:00
+ */
+@Repository
+@Lazy(false)
+public class OldDataCleaner {
+    private final MongoTemplate mongoTemplate;
+
+    public OldDataCleaner(MongoTemplate mongoTemplate) {
+        this.mongoTemplate = mongoTemplate;
+    }
+
+    @PostConstruct
+    public void main() {
+        CompletableFuture.runAsync(() -> cleanOldData(mongoTemplate));
+    }
+
+    public void cleanOldData(MongoTemplate mongoTemplate) {
+        cleanConfigComparisonIgnoreCategoryCollection(mongoTemplate);
+    }
+
+    private void cleanConfigComparisonIgnoreCategoryCollection(MongoTemplate mongoTemplate) {
+        Query query = Query.query(Criteria.where(ConfigComparisonIgnoreCategoryCollection.Fields.ignoreCategory).ne(null));
+        List<ConfigComparisonIgnoreCategoryCollection> oldData =
+            mongoTemplate.findAllAndRemove(query, ConfigComparisonIgnoreCategoryCollection.class).stream()
+                .filter(config -> CollectionUtils.isNotEmpty(config.getIgnoreCategory()))
+                .collect(Collectors.toList());
+
+        List<ConfigComparisonIgnoreCategoryCollection> newData = new ArrayList<>();
+        oldData.forEach(oldConfig -> {
+            oldConfig.getIgnoreCategory().forEach(ignoreCategory -> {
+                ConfigComparisonIgnoreCategoryCollection newConfigItem = convertToNewConfig(oldConfig);
+                CategoryDetailDao categoryDetailDao = new CategoryDetailDao();
+                categoryDetailDao.setOperationType(ignoreCategory);
+                newConfigItem.setIgnoreCategoryDetail(categoryDetailDao);
+
+                newData.add(newConfigItem);
+            });
+        });
+        mongoTemplate.insertAll(newData);
+    }
+
+    private ConfigComparisonIgnoreCategoryCollection convertToNewConfig(
+        ConfigComparisonIgnoreCategoryCollection oldConfig) {
+        ConfigComparisonIgnoreCategoryCollection newConfig = new ConfigComparisonIgnoreCategoryCollection();
+        MongoHelper.initInsertObject(newConfig);
+        newConfig.setAppId(oldConfig.getAppId());
+        newConfig.setOperationId(oldConfig.getOperationId());
+        newConfig.setExpirationType(oldConfig.getExpirationType());
+        newConfig.setExpirationDate(oldConfig.getExpirationDate());
+        return newConfig;
+    }
+}
