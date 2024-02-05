39667ffa8523bd14cf8fdd63e1880a096afb38dc
王雨疏 <35320326+wildeslam@users.noreply.github.com>
2024-02-05 19:22:16 +0800

    fix:mapstruct (#368)
    Co-authored-by: wildeslam <yushuwang@trip.com>

--- arex-web-model/src/main/java/com/arextest/web/model/mapper/CaseSummaryMapper.java
+++ arex-web-model/src/main/java/com/arextest/web/model/mapper/CaseSummaryMapper.java
@@ -2,6 +2,7 @@ package com.arextest.web.model.mapper;
 
 import com.arextest.web.model.dao.mongodb.iosummary.CaseSummaryCollection;
 import com.arextest.web.model.dto.iosummary.CaseSummary;
+import org.mapstruct.Builder;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
@@ -10,7 +11,7 @@ import org.mapstruct.factory.Mappers;
 /**
  * Created by rchen9 on 2023/2/28.
  */
-@Mapper
+@Mapper(builder = @Builder(disableBuilder = true))
 public interface CaseSummaryMapper {
 
   CaseSummaryMapper INSTANCE = Mappers.getMapper(CaseSummaryMapper.class);
