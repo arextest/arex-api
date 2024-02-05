101a6126bec19cc0c158b0539cb4830505850d04
wildeslam <yushuwang@trip.com>
2024-02-05 18:57:57 +0800

    fix:mapstruct

--- arex-web-model/src/main/java/com/arextest/web/model/mapper/SceneInfoMapper.java
+++ arex-web-model/src/main/java/com/arextest/web/model/mapper/SceneInfoMapper.java
@@ -11,6 +11,7 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import org.mapstruct.AfterMapping;
+import org.mapstruct.Builder;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.factory.Mappers;
@@ -18,7 +19,7 @@ import org.mapstruct.factory.Mappers;
 /**
  * Created by rchen9 on 2023/2/28.
  */
-@Mapper
+@Mapper(builder = @Builder(disableBuilder = true))
 public interface SceneInfoMapper {
 
   SceneInfoMapper INSTANCE = Mappers.getMapper(SceneInfoMapper.class);
