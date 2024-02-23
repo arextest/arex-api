9e1350d8731a6a9ccfee0cb0ecf586f33cab9d54
Rui Chen <44330697+coryhh@users.noreply.github.com>
2024-02-23 14:33:54 +0800

    feat: refresh ComparisonIgnoreCategory (#375)

--- arex-web-model/src/main/java/com/arextest/web/model/dao/mongodb/entity/CategoryDetailDao.java
+++ arex-web-model/src/main/java/com/arextest/web/model/dao/mongodb/entity/CategoryDetailDao.java
@@ -1,12 +1,16 @@
 package com.arextest.web.model.dao.mongodb.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * @author wildeslam.
  * @create 2024/1/5 15:03
  */
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CategoryDetailDao {
 
       private String operationType;
