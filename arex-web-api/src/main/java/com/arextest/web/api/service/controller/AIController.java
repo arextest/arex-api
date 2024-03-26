0802232d81f7e07f6747c41216c366052c8779e4
qzmo <qzmo@trip.com>
2024-03-26 11:35:00 +0800

    feat: google ai

--- arex-web-api/src/main/java/com/arextest/web/api/service/controller/AIController.java
+++ arex-web-api/src/main/java/com/arextest/web/api/service/controller/AIController.java
@@ -1,11 +1,5 @@
 package com.arextest.web.api.service.controller;
 
-import com.arextest.common.model.response.Response;
-import com.arextest.common.utils.ResponseUtils;
-import com.arextest.web.core.business.ai.AI;
-import com.arextest.web.model.contract.contracts.vertexai.GenReq;
-import lombok.Data;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,13 +7,20 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.arextest.common.model.response.Response;
+import com.arextest.common.utils.ResponseUtils;
+import com.arextest.web.core.business.ai.AI;
+import com.arextest.web.model.contract.contracts.vertexai.GenReq;
+
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author: QizhengMo
  * @date: 2024/3/25 15:15
  */
 @Slf4j
 @Controller
-@RequestMapping("/ai/")
+@RequestMapping("/api/ai/")
 @CrossOrigin(origins = "*", maxAge = 3600)
 public class AIController {
 
