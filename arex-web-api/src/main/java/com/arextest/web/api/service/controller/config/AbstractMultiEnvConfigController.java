dbec60534ef3eb835abed4f1ba12c137f6c15aab
rchen9 <rchen9@trip.com>
2024-07-26 14:51:54 +0800

    feat: add log

--- arex-web-api/src/main/java/com/arextest/web/api/service/controller/config/AbstractMultiEnvConfigController.java
+++ arex-web-api/src/main/java/com/arextest/web/api/service/controller/config/AbstractMultiEnvConfigController.java
@@ -1,23 +1,25 @@
 package com.arextest.web.api.service.controller.config;
 
+import com.arextest.common.annotation.AppAuth;
+import com.arextest.common.model.response.Response;
+import com.arextest.common.utils.ResponseUtils;
 import com.arextest.config.model.dto.AbstractMultiEnvConfiguration;
+import com.arextest.config.model.dto.ModifyType;
+import com.arextest.web.core.business.config.MultiEnvConfigurableHandler;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.arextest.common.annotation.AppAuth;
-import com.arextest.common.model.response.Response;
-import com.arextest.common.utils.ResponseUtils;
-import com.arextest.config.model.dto.ModifyType;
-import com.arextest.web.core.business.config.MultiEnvConfigurableHandler;
-
-import lombok.Getter;
-
+@Slf4j
 @Getter
 public abstract class AbstractMultiEnvConfigController<T extends AbstractMultiEnvConfiguration<T>>
     extends AbstractConfigurableController<T> {
+
   protected MultiEnvConfigurableHandler<T> multiEnvConfigurableHandler;
+
   protected AbstractMultiEnvConfigController(MultiEnvConfigurableHandler<T> configurableHandler) {
     super(configurableHandler);
     this.multiEnvConfigurableHandler = configurableHandler;
@@ -29,10 +31,15 @@ public abstract class AbstractMultiEnvConfigController<T extends AbstractMultiEn
   @Override
   public Response modify(@PathVariable ModifyType modifyType, @RequestBody T configuration)
       throws Exception {
+    Response response;
     if (modifyType == ModifyType.UPDATE_MULTI_ENV) {
       configuration.validateEnvConfigs();
-      return ResponseUtils.successResponse(getMultiEnvConfigurableHandler().editMultiEnvList(configuration));
+      response = ResponseUtils.successResponse(
+          getMultiEnvConfigurableHandler().editMultiEnvList(configuration));
+    } else {
+      response = super.modify(modifyType, configuration);
     }
-    return super.modify(modifyType, configuration);
+    LOGGER.info("[[title=config.modify]]modify config, modifyType: {}, request: {}", configuration, response);
+    return response;
   }
 }
